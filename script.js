ecce7e3309f5454fb7db0bdcc34562f9a895dd9a
SerhiiTekhnev <seolteh@gmail.com>
2021-03-19 15:29:21 +0200

    FadeOut by checkbox

--- script.js
+++ script.js
@@ -2,30 +2,60 @@
 
 $('form').on('click', 'input[type=submit]', function(e) {
     e.preventDefault();
-    
     let task = $('input[type=text]').val();
-    
 
     if (task.length > 0) {
-
-
         $('ul.task-list').append('<li><input type=checkbox><p>' + task + '</p><img src="close.png" alt="close"></li>')
         $('input[type=text]').val('');
     }
 
     $(function (){
-            $('input[type=checkbox]').click(function(){
-                $(this).parent('li').remove();
-            });
-        
+        $('input[type=checkbox]').click(function(){
+            $(this).parent('li').fadeTo(1000,0.5);
         });
+    });  
+        
+        
 
+    
+//     $(function (){
+//             $('input[type=checkbox]').click(function(){
+//                 $(this).parent('li').remove();
+//             });
+        
+//         });
         
 })
 
 
-// $('ul.task-list').append('<li><input type=checkbox>' + task + '</li>')
+
+// $('form').on('click', 'input[type=submit]', function(e) {
+//     e.preventDefault();
+    
+//     let task = $('input[type=text]').val();
+    
+
+//     if (task.length > 0) {
+
+
+//         $('ul.task-list').append('<li><input type=checkbox><p>' + task + '</p><img src="close.png" alt="close"></li>')
 //         $('input[type=text]').val('');
+//     }
+
+//     $(function (){
+//             $('input[type=checkbox]').click(function(){
+//                 $(this).parent('li').remove();
+//             });
+        
+//         });
+
+        
+// })
+
+
+
+
+
 
 // $('.hover-edit-menu').on('click', '.remove-button', function(){
 //     var elem = $(this).closest('.item', '.todo-list');
